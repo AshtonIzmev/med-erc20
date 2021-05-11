6ea881cb4f6faab425878f537084edf2ef183f45
Issam <biossamu@gmail.com>
2021-05-11 08:34:11 +0000

    Add 'Mudaraba' product + a generic abstract product contract

--- sol/truffle/contracts/token/bank/DAT.sol
+++ sol/truffle/contracts/token/bank/DAT.sol
@@ -2,46 +2,28 @@
 
 pragma solidity ^0.8.0;
 
-import "../../utils/Context.sol";
-import "../../utils/EnumerableSet.sol";
-import "../med/MED.sol";
-import "../nft/FP.sol";
+import "./GenericProduct.sol";
 
 /**
  * @dev Banking smart contracts - Term Deposit (Depot A Terme)
  * @author AshtonIzmev
  */
-contract DAT is Context {
+contract DAT is GenericProduct {
 
     using EnumerableSet for EnumerableSet.UintSet;
-
-    MED public medToken;
-    FP public fpToken;
 
     uint256 public minimumAmount;
     uint256 public dayDuration;
     uint16 public interestRate;
 
-    address _issuingBank;
-
     struct Product {
         uint256 _subscriptionDate;
-        uint256 _subscriptionPrice;
+        uint256 _subscriptionAmount;
     }
 
     mapping (uint256 => Product) private _subscriptions;
-    EnumerableSet.UintSet private _subscriptionIds;
-
-    modifier onlyIssuingBank() {
-        require(
-            _msgSender() == _issuingBank,
-            "Only Issuing Bank is allowed to call this"
-        );
-        _;
-    }
 
     /**
-     * The treasure account is the "root" account on this currency
      * param minimumAmount      minimum amount deposited
      * param dayDuration        duration of the DAT (in days)
      * param interestRate       interest rate of the DAT
@@ -75,7 +57,7 @@ contract DAT is Context {
      */
     function cancelDat(uint256 tokenId) public virtual {
         require(fpToken.ownerOf(tokenId) == _msgSender());
-        uint256 initialAmount = _subscriptions[tokenId]._subscriptionPrice;
+        uint256 initialAmount = _subscriptions[tokenId]._subscriptionAmount;
         medToken.transfer(_msgSender(), initialAmount);
         fpToken.destroy(tokenId);
         _subscriptionIds.remove(tokenId);
@@ -87,23 +69,10 @@ contract DAT is Context {
     function payDat(uint256 tokenId) public virtual {
         require(medToken.daysElapsed() - _subscriptions[tokenId]._subscriptionDate > dayDuration, 
             "Too early to be payed");
-        uint256 initialAmount = _subscriptions[tokenId]._subscriptionPrice;
+        uint256 initialAmount = _subscriptions[tokenId]._subscriptionAmount;
         medToken.transfer(fpToken.ownerOf(tokenId), initialAmount * (100+interestRate) / 100);
         fpToken.destroy(tokenId);
         _subscriptionIds.remove(tokenId);        
-    }
-
-    function isSuscribed(uint256 tokenId) public virtual view returns (bool) {
-        return _subscriptionIds.contains(tokenId);
-    }
-
-    function getSubscription(uint256 idx) public virtual view returns (uint256) {
-        require(idx < _subscriptionIds.length(), "Idx overflow");
-        return _subscriptionIds.at(idx);
-    }
-
-    function getSubscriptionLength() public virtual view returns (uint256) {
-        return _subscriptionIds.length();
     }
 
 }
